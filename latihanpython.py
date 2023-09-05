7fda5bc93c81ab709325da68a1edd68d42fbf6b8
shamim0387 <144081099+shamim0387@users.noreply.github.com>
2023-09-05 12:52:36 +0800

    latihanpython

---
+++ latihanpython.py
@@ -0,0 +1,8 @@
+def print_hi():
+    pelajar = {"Name": "Shamim", "No Matrix": "18DDT21F2029"}
+    print(pelajar["Name"])
+    print(pelajar["No Matrix"])
+
+
+if __name__ == '__main__':
+    print_hi()
